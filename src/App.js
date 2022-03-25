67b7ee1d9bcf8593855f7f3d39f0c060938a87a6
wing <wongwingchit@gmail.com>
2022-03-24 22:59:58 -0700

    added board

--- src/App.js
+++ src/App.js
@@ -2,22 +2,60 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const renderMyGrid = () => {
+    var rowArray = [];
+    for(let i = 0; i< 10; i++){
+      var colArray = [];
+      for(let j=0; j< 10 ; j++){
+        colArray.push(
+          <div style = {{border : "1px solid", width: '3vw', height: '3vw', display: 'flex'}}>
+            {/* <div style={{ borderRadius: "50%", backgroundColor: 'white', display: 'flex', width: '70%', height: '70%', marginLeft: '10px', marginTop: '10px', padding: 1 }}>
+            </div> */}
+          </div>
+        )
+      }
+      rowArray.push(
+        <div style = {{ display: 'flex'}}>
+            {colArray}
+          </div>
+      )
+    }
+    return rowArray;
+  }
+  const renderOpponentsGrid = () => {
+    var rowArray = [];
+    for(let i = 0; i< 10; i++){
+      var colArray = [];
+      for(let j=0; j< 10 ; j++){
+        colArray.push(
+          <div style = {{border : "1px solid", width: '3vw', height: '3vw', display: 'flex'}}>
+            
+          </div>
+        )
+      }
+      rowArray.push(
+        <div style = {{ display: 'flex', marginLeft : "20vw"}}>
+            {colArray}
+          </div>
+      )
+    }
+    return rowArray;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+
+      <div style = {{ display: 'flex'}}>
+        <div> 
+          <div>my board</div>
+          <div>{renderMyGrid()}</div>
+        </div>
+        <div>
+          <div>opponents board</div>
+          <div>{renderOpponentsGrid()}</div>  
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
