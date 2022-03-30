0bd7484b1a5fe61fbff26da207e4dba55d0aea3f
wing <wongwingchit@gmail.com>
2022-03-29 23:02:53 -0700

    hook and handle click

--- src/App.js
+++ src/App.js
@@ -1,7 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  var row = [];
+  for(let i = 0; i< 10; i++){
+    var col = [];
+    for(let j=0; j< 10 ; j++){
+      col.push(null)
+    }
+    row.push(col);
+  }
+  
+  const [placeShips, setPlaceShips] = useState(
+    {
+      carrier:false,
+      battleship:false,
+      cruiser:false,
+      destroyer:false
+    })
+  const [pickShip, setPickShip] = useState(
+    {
+      carrier:false,
+      battleship:false,
+      cruiser:false,
+      destroyer:false
+    })
+  const [board, setBoard] = useState(row)
+  const handlePlaceCarrier = () =>{
+    if(!placeShips.carrier){
+      setPickShip({
+        carrier:true,
+        battleship:false,
+        cruiser:false,
+        destroyer:false
+      })
+    }
+    
+  }
+  const handlePlaceBattleship = () =>{
+    if(!placeShips.battleship){
+      setPickShip({
+      carrier:false,
+      battleship:true,
+      cruiser:false,
+      destroyer:false
+    })
+  }
+}
+  const handlePlaceCruiser = () =>{
+    if(!placeShips.cruiser){
+      setPickShip({
+      carrier:false,
+      battleship:false,
+      cruiser:true,
+      destroyer:false
+    })
+  }
+}
+  const handlePlaceDestroyer = () =>{
+    if(!placeShips.destroyer){
+      setPickShip({
+      carrier:false,
+      battleship:false,
+      cruiser:false,
+      destroyer:true
+    })
+  }
+}
   const renderMyGrid = () => {
     var rowArray = [];
     for(let i = 0; i< 10; i++){
@@ -9,8 +75,7 @@ function App() {
       for(let j=0; j< 10 ; j++){
         colArray.push(
           <div style = {{border : "1px solid", width: '3vw', height: '3vw', display: 'flex'}}>
-            {/* <div style={{ borderRadius: "50%", backgroundColor: 'white', display: 'flex', width: '70%', height: '70%', marginLeft: '10px', marginTop: '10px', padding: 1 }}>
-            </div> */}
+          
           </div>
         )
       }
@@ -34,7 +99,7 @@ function App() {
         )
       }
       rowArray.push(
-        <div style = {{ display: 'flex', marginLeft : "20vw"}}>
+        <div style = {{ display: 'flex'}}>
             {colArray}
           </div>
       )
@@ -43,15 +108,23 @@ function App() {
   }
   return (
     <div className="App" >
-
+      {console.log(board)}
       <div style = {{ display: 'flex'}}>
         <div> 
           <div>my board</div>
           <div>{renderMyGrid()}</div>
+          <div onClick={handlePlaceCarrier}> carrier </div>
+          <div onClick={handlePlaceBattleship}> battleship </div>
+          <div onClick={handlePlaceCruiser}> cruiser </div>
+          <div onClick={handlePlaceDestroyer}> destroyer </div>
         </div>
-        <div>
+        <div style = {{  marginLeft : "20vw"}}>
           <div>opponents board</div>
-          <div>{renderOpponentsGrid()}</div>  
+          <div>{renderOpponentsGrid()}</div> 
+          <div> carrier </div>
+          <div> battleship </div>
+          <div> cruiser </div>
+          <div> destroyer </div>
         </div>
         
       </div>
